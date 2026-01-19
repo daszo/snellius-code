158a4ce8106075c4b9f0033f32124809cebc2d94
daszo <daniel@vanoosteroom.com>
2026-01-19 19:25:54 +0100

    .

--- pipelines/pre_pipeline_same_mid_thread.sh
+++ pipelines/pre_pipeline_same_mid_thread.sh
@@ -15,28 +15,29 @@ echo "Starting pipeline"
 echo "1. data cleaning and textrank"
 export PYTHONUNBUFFERED=1
 
-PYTHONUNBUFFERED=1 /gpfs/work5/0/prjs1828/DSI-QG/.venv/bin/python3 -m pipelines.run_stage1 --thread
+PYTHONUNBUFFERED=1 /gpfs/work5/0/prjs1828/DSI-QG/.venv/bin/python3 -m pipelines.run_stage1 --thread --table_from "N10k" --table_to "N10k_thread_same_mid" destination_table "N10k_thread_same_mid_tr"
 
 RETURN_SEQ=1
 
 echo "2. Title generation"
 
-# PYTHONUNBUFFERED=1 /gpfs/work5/0/prjs1828/DSI-QG/.venv/bin/torchrun --nproc_per_node=1 run.py \
-#     --do_predict \
-#     --task generation \
-#     --table_name v_CleanMessages_thread \
-#     --model_name /gpfs/work5/0/prjs1828/DSI-QG/local_models/t5-headline \
-#     --per_device_eval_batch_size 32 \
-#     --run_name docTquery-full-generation \
-#     --max_length 256 \
-#     --valid_file /data/enron.db \
-#     --output_dir temp \
-#     --dataloader_num_workers 10 \
-#     --report_to wandb \
-#     --logging_steps 100 \
-#     --num_return_sequences $RETURN_SEQ \
-#     --thread 1
+PYTHONUNBUFFERED=1 /gpfs/work5/0/prjs1828/DSI-QG/.venv/bin/torchrun --nproc_per_node=1 run.py \
+    --do_predict \
+    --task generation \
+    --table_name N10k_thread_same_mid_tr \
+    --model_name /gpfs/work5/0/prjs1828/DSI-QG/local_models/t5-headline \
+    --per_device_eval_batch_size 32 \
+    --run_name docTquery-full-generation \
+    --max_length 256 \
+    --valid_file /data/enron.db \
+    --output_dir temp \
+    --dataloader_num_workers 10 \
+    --report_to wandb \
+    --logging_steps 100 \
+    --num_return_sequences $RETURN_SEQ \
+    --thread 1
+--same_mid N10k_thread_same_mid
 
 echo "3. splitsing the data"
 
-PYTHONUNBUFFERED=1 /gpfs/work5/0/prjs1828/DSI-QG/.venv/bin/python3 -m pipelines.prep_datasets --thread "full_thread"
+# PYTHONUNBUFFERED=1 /gpfs/work5/0/prjs1828/DSI-QG/.venv/bin/python3 -m pipelines.prep_datasets --thread "N10k_thread_same_mid"
--- run.py
+++ run.py
@@ -414,7 +414,7 @@ def main():
 
 
         evaluator = DSIEmailSearchEvaluator(
-            trainer=trainer, run_args=run_args, input_file=run_args.test_file, df=df
+            trainer=trainer, run_args=run_args, input_file=run_args.test_file, df=df, tokenizer=tokenizer
         )
 
         print("Preparing Data...")
